3abc8052b84cba9cae2b0c7f64af38ef836c6aac
Benedikt Meurer <benedikt.meurer@googlemail.com>
2011-06-10 17:39:36 +0200

    Initial Commit

---
+++ ClassBrowser/ClassBrowserAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  ClassBrowserAppDelegate.h
+//  ClassBrowser
+//
+//  Created by Benedikt Meurer on 6/10/11.
+//  Copyright 2011 Universität Siegen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ClassBrowserAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
